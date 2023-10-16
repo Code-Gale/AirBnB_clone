0ec7dea067285ae207b3a0da9a5c1d77953eca05
Code-Gale <codewhiz001@gmail.com>
2023-10-16 13:08:38 +0100

    console wahala

--- console.py
+++ console.py
@@ -3,14 +3,10 @@
 """
 
 import cmd
-from models import storage
-from models.amenity import Amenity
 from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from models import storage
+import re
+import json
 
 
 class HBNBCommand(cmd.Cmd):
